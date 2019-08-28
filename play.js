de46f53a1f096794af109a45fbabc2bee040496e
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 20:03:51 +0000

    forgot to lint play.js

--- play.js
+++ play.js
@@ -2,20 +2,20 @@ const net = require('net');
 
 //  make a connection with the game server
 
- const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function() {
+  const conn = net.createConnection({
     host: '192.168.88.151',
     port: 50541
   });
-//   handles incoming data and console logs it
+  //   handles incoming data and console logs it
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
   // turn data into text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   return conn;
-}
+};
 
 console.log('Connecting ...');
 connect();
