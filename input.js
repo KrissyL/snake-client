d39351a4acb32a18cec2aa235df4ad5b27600730
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 21:53:01 +0000

    refactored and exported setupInputs

---
+++ input.js
@@ -0,0 +1,18 @@
+// makes sure you're able to exit with ctrl+c once in the game
+const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  // setup User Interface to handle input through stdin
+  const setupInput = function() {
+  
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding('utf8');
+    stdin.resume();
+    stdin.on('data', handleUserInput);
+    return stdin;
+  };
+
+  module.exports.setupInput = setupInput;
