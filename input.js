c6625ebb8ded9f4041f7819bab81b26194a334be
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 22:29:37 +0000

    added the move functions on connection

--- input.js
+++ input.js
@@ -1,18 +1,35 @@
+// Stores the active TCP connection object.
+let connection;
+
 // makes sure you're able to exit with ctrl+c once in the game
 const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+};
   // setup User Interface to handle input through stdin
-  const setupInput = function() {
+const setupInput = function(conn) {
+    
+  connection = conn;
   
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding('utf8');
-    stdin.resume();
-    stdin.on('data', handleUserInput);
-    return stdin;
-  };
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
 
-  module.exports.setupInput = setupInput;
+module.exports.setupInput = setupInput;
