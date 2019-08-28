fd598c2127f6553b68b37343063a2584924ef054
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 20:18:12 +0000

    refactored to create a separate module for tcp connection

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+
+//  make a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.88.151',
+    port: 50541
+  });
+  //   handles incoming data and console logs it
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  // turn data into text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+// export the function using ES6 object shorthand
+module.exports.connect = connect;
--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require('net');
-
-//  make a connection with the game server
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.88.151',
-    port: 50541
-  });
-  //   handles incoming data and console logs it
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  // turn data into text
-  conn.setEncoding('utf8');
-
-  return conn;
-};
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
