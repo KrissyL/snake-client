519d99543b4726edfe74c4fde4e406026171549a
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 21:47:33 +0000

    added an event handler to deal with key press to exit program

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+// makes sure you're able to exit with ctrl+c once in the game
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+// setup User Interface to handle input through stdin
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+setupInput();
