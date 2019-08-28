286ae7cd4cbd3357e879b3f5bd033e742d40469a
Krissy Lewis <kristianlewis03@gmail.com>
2019-08-28 21:07:40 +0000

    learning the Move:__ commands

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-//  make a connection with the game server
+// make a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
     host: '192.168.88.151',
@@ -10,8 +10,10 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('You are now connected to the game server');
     conn.write('Name: N3R');
-  }),
-  //   handles incoming data and console logs it
+    // Writes Move: Up message to the server on connection
+    //conn.write('Move: up');
+  });
+  // handles incoming data and console logs it
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
